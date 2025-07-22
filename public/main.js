e75c05a1e5649ca54d9edca2482ff59fa3e78f31
Jamesrkiv <73561665+Jamesrkiv@users.noreply.github.com>
2025-07-22 08:12:39 -0400

    Formatting tweaks

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ async function loadData() {
 	grid.innerHTML = '';
 	data.forEach(n => {
 		grid.innerHTML +=`
-			<div class="text-[#43434E] border border-gray-300 rounded-md py-2 px-4 shadow text-center text-2xl font-semibold break-words flex-shrink-0 max-w-full">
+			<div class="text-[#43434E] border border-gray-300 bg-white rounded-md py-2 px-4 shadow text-center text-2xl font-semibold break-words flex-shrink-0 max-w-full">
 				${n.name}
 			</div>
 		`;
